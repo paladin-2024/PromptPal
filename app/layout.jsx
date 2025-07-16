e309f2a3554a173a56c065c563884db4f44d41f3
paladin-2024 <cnzabb@gmail.com>
2025-07-16 06:11:43 +0300

    {     "error": {         "message": "You didn't provide an API key. You need to provide your API key in an Authorization header using Bearer auth (i.e. Authorization: Bearer YOUR_KEY), or as the password field (with blank username) if you're accessing the API from your browser and are prompted for a username and password. You can obtain an API key from https://platform.openai.com/account/api-keys.",         "type": "invalid_request_error",         "param": null,         "code": null     } }

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,6 @@
 import "@styles/globals.css";
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
 
 export const metadata = {
     title: "PromptPal",
@@ -13,7 +15,7 @@ const RootLayout = ({ children }) => (
         </div>
 
         <main className='app'>
-
+            <Nav/>
             {children}
         </main>
     </body>
---
+++ components/Feed.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Feed = () => {
+    return (
+        <div>Fuck you</div>
+    )
+}
+export default Feed
