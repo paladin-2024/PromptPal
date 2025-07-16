724947369226a1106a61610da602e1534d9cb67c
paladin-2024 <cnzabb@gmail.com>
2025-07-16 17:19:37 +0300

    {     "error": {         "message": "You didn't provide an API key. You need to provide your API key in an Authorization header using Bearer auth (i.e. Authorization: Bearer YOUR_KEY), or as the password field (with blank username) if you're accessing the API from your browser and are prompted for a username and password. You can obtain an API key from https://platform.openai.com/account/api-keys.",         "type": "invalid_request_error",         "param": null,         "code": null     } }

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,9 +1,81 @@
+'use client'
 import Link from 'next/link'
 import Image from 'next/image'
 import {useState, useEffect} from 'react'
+import { signIn, signOut, getProviders
+} from "next-auth/react";
+
 const Nav = () => {
+    const isUserLoggedIn = true;
+    const [providers, setProviders]= useState(null);
+    useEffect(() => {
+        const setProviders = async ()=>{
+            const response = await getProviders();
+            setProviders(response);
+        }
+        setProviders();
+    }, []);
     return (
-        <div></div>
+        <nav className='flex-between w-full mb-16 pt-3'>
+            <Link href='/' className='flex gap-2 flex-center'>
+                <Image src='/assets/images/logo.svg' alt='PromptPal Logo' width={30} height={30} className='object-contain'/>
+                <p className='logo_text'>PromptPal</p>
+            </Link>
+
+            <div className='sm:flex hidden'>
+                {isUserLoggedIn ? (
+                    <di className='flex gap-3 md:gap-5'>
+                        <Link href="/create-prompt" className="black_btn">
+                            Create Post
+                        </Link>
+
+                        <button type="button" onClick={signOut} className='outline_btn'>
+                            Sign Out
+                        </button>
+                        <Link href="/profile">
+                            <Image src="/assets/images/logo.svg"
+                               width={37} height={37}
+                               className="rounded-full"
+                               alt="profile"
+                            />
+                        </Link>
+                    </di>
+                ): (
+                    <>
+                        {providers && Object.values(providers).map((provider) => (
+                            <button type="button" key={provider.name}
+                                    onClick={()=> signIn(provider.id)}
+                                    className='black_btn'>
+
+                            </button>
+                        ))}
+                    </>
+                )}
+            </div>
+            <div className='sm:hidden flex relative'>
+                {isUserLoggedIn ? (
+                    <div className="">
+                        <Image src="/assets/images/logo.svg"
+                               width={37} height={37}
+                               className="rounded-full"
+                               alt="profile"
+                               onClick={()=>{}}
+                        />
+                    </div>
+                ):(
+                    <>
+                        {providers && Object.values(providers).map((provider) => (
+                            <button type="button" key={provider.name}
+                                    onClick={()=> signIn(provider.id)}
+                                    className='black_btn'>
+
+                            </button>
+                        ))}
+                    </>
+                )}
+
+            </div>
+        </nav>
     )
 }
 export default Nav
