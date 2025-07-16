aae8aabe8476151f53018a6c6b07973ce6ea9b59
paladin-2024 <cnzabb@gmail.com>
2025-07-16 05:38:25 +0300

    Add MongoDB, Mongoose, bcrypt, and next-auth dependencies

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+    return (
+        <section className='w-full flex-center flex-col'>
+            <h1 className='head_text text-center'>
+                Discover & share
+                <br className='max-md:hidden' />
+                <span className='font-bold text-2xl md:text-3xl md:text-4xl'>AI-Powered Prompts</span>
+            </h1>
+        </section>
+    )
+}
+export default Home
