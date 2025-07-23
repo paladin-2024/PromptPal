4626b00b4f81d976afe065191d82bfeb4c4549c2
paladin-2024 <cnzabb@gmail.com>
2025-07-23 19:25:20 +0300

    ✨ Fix typo in MongoDB connection options and update username validation regex

--- utils/database.js
+++ utils/database.js
@@ -11,7 +11,7 @@ export const connectToDB= async() =>{
     try {
         await mongoose.connect(process.env.MONGODB_URI,{
             dbName:"share_prompt",
-            useNewUrlParse:true,
+            useNewUrlParser:true,
             useUnifiedTopology:true,
         })
         isConnected = true;
